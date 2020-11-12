908438b9be17e7430e359e16b8e3d8f0475d0f44
maedashosuke <shosukemaeda.1011@gmail.com>
2020-11-12 07:13:41 +0000

    [Add] 課題

--- bookers_app/app/controllers/books_controller.rb
+++ bookers_app/app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -15,9 +16,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error"
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
@@ -25,9 +32,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:alert] = "error"
+      render 'edit'
+    end
   end
 
   def destroy
